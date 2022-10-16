4e35fa1cb27afe14ae770d99237f4ebd714fec69
Ehsan Pourhadi <ehsan.po@gmail.com>
2022-10-16 09:06:22 +0200

    hero fix

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -4,13 +4,13 @@ import Button from "../Button";
 import logo from "./logo.svg";
 const Hero = () => {
   return (
-    <section className="hero">
+    <section className={style.hero}>
       <div className="wrapper">
         <div className="v-center--f">
-          <img alt="EP Logo" className="logo" src="logo.svg" />
+          <img alt="EP Logo" className={style.logo} src="logo.svg" />
           <h1 className="display">Ehsan Pourhadi</h1>
           <h2>Software Developer / Music Producer</h2>
-          <div className="intro">
+          <div className={style.intro}>
             <p>
               I'm a Developer, Designer, and Producer who provides digital
               solutions with more than ten years of experience creating and
@@ -26,10 +26,10 @@ const Hero = () => {
           </Button>
         </div>
       </div>
-      <div className="container-s">
-        <div className="chevron"></div>
-        <div className="chevron"></div>
-        <div className="chevron"></div>
+      <div className={style.container__s}>
+        <div className={style.chevron}></div>
+        <div className={style.chevron}></div>
+        <div className={style.chevron}></div>
       </div>
     </section>
   );
