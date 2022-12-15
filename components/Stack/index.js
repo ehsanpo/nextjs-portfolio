b188d9773f0145755319efaff660db3f6efbe7f0
Ehsan Pourhadi <ehsan.po@gmail.com>
2022-12-15 17:15:55 +0100

    stacks

--- components/Stack/index.js
+++ components/Stack/index.js
@@ -1,43 +1,26 @@
 import React from "react";
 import style from "./Stack.module.scss";
+import stacks_data from "../../data/stacks-data";
 
-const Stack = ({ type = "nodejs" }) => {
-  const types = {
-    LNMP: {
-      headline: "LNMP",
-      tools: ["Linux", "Nginx", "Mysql", "PHP"],
-      icon: "/servers.svg",
-    },
-    nodejs: {
-      headline: "Node.js",
-      tools: ["React", "Gastby", "Webpack", "MongoDb"],
-      icon: "/nodejs.svg",
-    },
-    cloud: {
-      headline: "Cloud",
-      tools: ["AWS API Gateway", "AWS Dynamo DB", "Lambda", "Node.js"],
-      icon: "/cs.svg",
-    },
-    wordpress: {
-      headline: "Wordpres",
-      tools: ["Blockpress Theme", "Content Blocks", "Sass", "Webpack"],
-      icon: "/wordpress.svg",
-    },
-  };
-  let toRender = types[type];
-
+const Stack = () => {
   return (
-    <div className={style.stackholder}>
-      <img alt={toRender.headline} src={toRender.icon} />
-      <h3>{toRender.headline}</h3>
-      <div className={style.stacks}>
-        <ul>
-          {toRender.tools.map((t) => (
-            <li key={t}>{t}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <>
+      {stacks_data.map((Xstack) => {
+        return (
+          <div className={style.stackholder}>
+            <img alt={Xstack.slug} src={Xstack.icon} />
+            <h3>{Xstack.slug}</h3>
+            <div className={style.stacks}>
+              <ul>
+                {Xstack.labels.map((t) => (
+                  <li key={t}>{t}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
