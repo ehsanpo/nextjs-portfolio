f1283478d248c8b0b2d098141e7f0f9e9a87b2b4
Ehsan Pourhadi <ehsan.po@gmail.com>
2023-07-18 11:05:47 +0200

    css fixes

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,24 +1,20 @@
-const sgMail = require("@sendgrid/mail");
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 export default async (req, res) => {
   const { name, message } = req.body;
 
-  const mail = {
-    from: "ehsan.po@gmail.com",
-    to: "ehsan.po@gmail.com",
-    subject: "New message from contact form",
-    text: message,
-    html: message,
-  };
+  // const mail = {
+  //   from: "ehsan.po@gmail.com",
+  //   to: "ehsan.po@gmail.com",
+  //   subject: "New message from contact form",
+  //   text: message,
+  //   html: message,
+  // };
 
-  sgMail
-    .send(mail)
-    .then((response) => {
-      res.status(200).json({ message: response });
-    })
-    .catch((error) => {
-      res.status(400).json({ message: error });
-    });
+  // sgMail
+  //   .send(mail)
+  //   .then((response) => {
+  //     res.status(200).json({ message: response });
+  //   })
+  //   .catch((error) => {
+  //     res.status(400).json({ message: error });
+  //   });
 };
