ccdb3be4444888ea5d161de993357eb265160b9a
Ehsan Pourhadi <ehsan.pourhadi@telavox.com>
2024-01-05 14:42:55 +0000

    text and image

--- components/ImageAndText/index.js
+++ components/ImageAndText/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import style from "./imageAndText.module.scss";
 
-const ImageAndText = ({ image, children, nopad, left }) => (
-  <section className={style.noPad}>
+const ImageAndText = ({ image, children, nopad, left, noMarg }) => (
+  <section className={`${style.noPad}  ${noMarg ? style.noMarg : ""}`}>
     <div className="wrapper">
       <div className={` ${style.ImageAndText} ${left ? style.left : ""}`}>
         <div className={style.itb_image}>{image}</div>
