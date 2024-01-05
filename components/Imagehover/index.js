844ad6cfc8b5af176065077fdb6c7bdb6a9c957d
Ehsan Pourhadi <ehsan.pourhadi@telavox.com>
2024-01-05 14:43:02 +0000

    hover image

--- components/Imagehover/index.js
+++ components/Imagehover/index.js
@@ -1,43 +1,18 @@
 import React, { useRef, useEffect } from "react";
+// @ts-ignore
 import hoverEffect from "hover-effect";
-// import portfolioImage from "../../public/images/ep-3-3.jpg";
-// import portfolioImage2 from "../../public/images/ep-3-2.jpg";
-import portfolioImage from "../../public/ehsan/111.jpg";
-import portfolioImage2 from "../../public/images/ep-3-2.jpg";
-
-import flip1 from "../../public/images/111.jpg";
-import flip2 from "../../public/images/112.jpg";
-import flip3 from "../../public/images/113.jpg";
-import flip4 from "../../public/images/114.jpg";
-import flip5 from "../../public/images/115.jpg";
-import flip6 from "../../public/images/116.jpg";
-import flip7 from "../../public/images/117.jpg";
-import flip8 from "../../public/images/118.jpg";
-import flip9 from "../../public/images/119.jpg";
-import flip10 from "../../public/images/120.jpg";
 
 const LocalImage = () => {
   const container = useRef();
-  const ImageEffectArray = [
-    "../../images/111.jpg",
-    "../../images/112.jpg",
-    "../../images/113.jpg",
-    "../../images/114.jpg",
-    "../../images/115.jpg",
-    "../../images/116.jpg",
-    "../../images/117.jpg",
-    "../../images/118.jpg",
-    "../../images/118.jpg",
-    "../../images/120.jpg",
-  ];
+  const ImageEffectArray = ["../img/111.jpg", "../img/112.jpg"];
   const randomElement =
     ImageEffectArray[Math.floor(Math.random() * ImageEffectArray.length)];
   useEffect(() => {
     var myAnimation = new hoverEffect({
       parent: container.current,
       intensity: 0.3,
-      image1: "../../ehsan/112.jpg",
-      image2: "../../ehsan/116.jpg",
+      image1: "../img/ehsan1.jpg",
+      image2: "../img/ehsan2.jpg",
       displacementImage: randomElement,
     });
   }, [container, randomElement]);
@@ -45,11 +20,14 @@ const LocalImage = () => {
   return (
     <div
       id="imgContainer"
+      className="clipPath2"
+      // @ts-ignore
       ref={container}
       style={{
-        width: 340,
-        height: 560,
+        width: 512,
+        height: 512,
         marginTop: 80,
+        maxWidth: "100%",
       }}
     />
   );
